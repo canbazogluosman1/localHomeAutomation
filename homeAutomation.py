01419c20b02f041147d17bfbbe28d3a9e737c3c4
canbazogluosman1 <canbazoglu.osman@gmail.com>
2024-05-17 14:45:10 +0300

    bütün islemler fonksiyonlara bölündü

--- homeAutomation.py
+++ homeAutomation.py
@@ -3,12 +3,14 @@ from vosk import Model, KaldiRecognizer
 import pyaudio
 import json
 from phi.assistant import Assistant
+from phi.llm.ollama import Ollama
 from typing import List
 from pydantic import BaseModel, Field
 from rich.pretty import pprint
-
+import time
+aktif_dinleme = False
 class FunctionScript(BaseModel):
-    functionName: list = Field(..., description="Görevi gerçekleştirmek için sadece gerekli fonksiyonlari listele(string). Lamba, kapi,polisiAra,ambulansiAra,robotSüpürgeyiCalistir veya pencere")
+    functionName: List[str] = Field(..., description="Görevi gerçekleştirmek için sadece gerekli fonksiyonları listele (string). Lamba, kapi, polisiAra, ambulansiAra, robotSüpürgeyiCalistir veya pencere")
     Value: str = Field(..., description="Görevi gerçekleştirmek için değer.")
 
 def Lamba(value):
@@ -29,68 +31,92 @@ def ambulansiAra(value):
 def robotSüpürgeyiCalistir(value):
     return "Robot süpürgeyi çalıştır: " + value
 
-func_assistant = Assistant(
-    description="Ev otomasyonu asistanısınız.",
-    output_model=FunctionScript,
-)
+def getMyAssistant(keep_alive):
+    func_assistant = Assistant(
+        #llm=Ollama(model="llama3", keep_alive=keep_alive),
+        description="Ev otomasyonu asistanısınız.",
+        output_model=FunctionScript,
+        monitoring=True,
+    )
+    return func_assistant
 
-# Sesi metne dönüştürme
-model_path = "./vosk-model-small-tr-0.3"
-if not os.path.exists(model_path):
-    print("Lütfen doğru model dizinini belirtiniz!")
-    exit(1)
+def getMyStream():
+    model_path = "./vosk-model-small-tr-0.3"
+    if not os.path.exists(model_path):
+        print("Lütfen doğru model dizinini belirtiniz!")
+        exit(1)
 
-model = Model(model_path)
+    model = Model(model_path)
 
-p = pyaudio.PyAudio()
-stream = p.open(format=pyaudio.paInt16,
-                channels=1,
-                rate=16000,
-                input=True,
-                frames_per_buffer=8000)
-stream.start_stream()
+    p = pyaudio.PyAudio()
+    stream = p.open(format=pyaudio.paInt16,
+                    channels=1,
+                    rate=16000,
+                    input=True,
+                    frames_per_buffer=8000)
+    stream.start_stream()
 
-recognizer = KaldiRecognizer(model, 16000)
+    recognizer = KaldiRecognizer(model, 16000)
+    return recognizer, stream, p
 
-print("cambaz demeden dinlemeyeceğim, dur dediğinizde duracağım...")
-
-aktif_dinleme = False  # Başlangıçta dinleme pasif olarak ayarlandi
-
-while True:
+def main(func_assistant=None, stream=None, recognizer=None, p=None):
+    global aktif_dinleme  
+    if not func_assistant or not stream or not recognizer:
+        return
+    
     data = stream.read(4000, exception_on_overflow=False)
     if recognizer.AcceptWaveform(data):
         result = json.loads(recognizer.Result())
         if 'text' in result:
             print(result['text'])  # Algılanan metni yazdır
             if "cambaz" in result['text']:
-                aktif_dinleme = True  # "cambaz" kelimesi iledinlemeyi aktif et
+                aktif_dinleme=True# "cambaz" kelimesi ile dinlemeyi aktif et
                 print("Artık dinliyorum...")
             elif "dur" in result['text'] and aktif_dinleme:
-                aktif_dinleme = False  # "Dur" kelimesi ile dinlemeyi pasif et
+                aktif_dinleme = False  # "dur" kelimesi ile dinlemeyi pasif et
                 print("Dinlemeyi durduruyorum, tekrar cambaz demelisiniz...")
             if aktif_dinleme:
-                answer = func_assistant.run(result['text'])
-
-                for f in answer.functionName:
-                    if f == "Lamba":
-                        pprint(Lamba(answer.Value))
-                    elif f == "pencere":
-                        pprint(pencere(answer.Value))
-                    elif f == "kapi":
-                        pprint(kapi(answer.Value))
-                    elif f == "polisiAra":
-                        pprint(polisiAra(answer.Value))
-                    elif f == "ambulansiAra":
-                        pprint(ambulansiAra(answer.Value))
-                    elif f == "robotSüpürgeyiCalistir":
-                        pprint(robotSüpürgeyiCalistir(answer.Value))
-
+                try:
+                    start = time.time()
+                    print("burasi result",result['text'])
+                    if result['text'] and result['text'] != "cambaz":
+                        answer = func_assistant.run(result['text'])
+                        print("answer",answer)
+                        for f in answer.functionName:
+                            if f == "Lamba":
+                                pprint(Lamba(answer.Value))
+                            elif f == "pencere":
+                                pprint(pencere(answer.Value))
+                            elif f == "kapi":
+                                pprint(kapi(answer.Value))
+                            elif f == "polisiAra":
+                                pprint(polisiAra(answer.Value))
+                            elif f == "ambulansiAra":
+                                pprint(ambulansiAra(answer.Value))
+                            elif f == "robotSüpürgeyiCalistir":
+                                pprint(robotSüpürgeyiCalistir(answer.Value))
+                except Exception as e:
+                    print("The error is: ", e)
+                    stopStream(stream, p)
+                finally:
+                    print(f'Time: {time.time() - start}')
         else:
             if aktif_dinleme:
                 partial_result = json.loads(recognizer.PartialResult())
                 if 'partial' in partial_result:
                     print("Dinleme: ", partial_result['partial'])
 
-stream.stop_stream()
-stream.close()
-p.terminate()
+def stopStream(stream, p):
+    stream.stop_stream()
+    stream.close()
+    p.terminate()
+
+if __name__ == '__main__':
+    recognizer, stream, p = getMyStream()
+    func_assistant = getMyAssistant(keep_alive=True)
+    print("cambaz demeden dinlemeyeceğim, dur dediğinizde duracağım...")
+    while True:
+        try:
+            main(func_assistant=func_assistant, stream=stream, recognizer=recognizer, p=p)
+        except Exception as e:
+            print("The error is: ", e)
